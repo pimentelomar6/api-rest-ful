11d51eba9481b069565224b4c9e33630566a4317
Omar Pimentel <pimentelomar6@gmail.com>
2023-03-23 01:31:01 -0400

    Testing Respository

--- src/test/java/com/pimentelprojects/api/repository/ClientRepositoryTest.java
+++ src/test/java/com/pimentelprojects/api/repository/ClientRepositoryTest.java
@@ -8,6 +8,8 @@ import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class ClientRepositoryTest {
@@ -36,5 +38,59 @@ public class ClientRepositoryTest {
         Assertions.assertThat(saveClient.getId()).isGreaterThan(0);
 
     }
+
+    @Test
+    public void ClientRepository_GetAll_ReturnMoreThenOneClient(){
+
+        //Arrange
+        Client client = Client.builder()
+                .name("Carlos")
+                .address("Avenida tachira")
+                .mail("carlos@gmail.com")
+                .phoneNumber("+5824598")
+                .build();
+
+        Client client2 = Client.builder()
+                .name("Juan")
+                .address("Calle 5 Room 23")
+                .mail("juan_g@gmail.com")
+                .phoneNumber("+58222598825")
+                .build();
+
+        clientRepository.save(client);
+        clientRepository.save(client2);
+
+        //Act
+
+        List<Client> clientList = clientRepository.findAll();
+
+        //Asserts
+        Assertions.assertThat(clientList).isNotNull();
+        Assertions.assertThat(clientList.size()).isEqualTo(2);
+
+    }
+
+    @Test
+    public void ClientRepository_FindById_ReturnClient(){
+        //Arrange
+        Client client = Client.builder()
+                .name("Carlos")
+                .address("Avenida tachira")
+                .mail("carlos@gmail.com")
+                .phoneNumber("+5824598")
+                .build();
+
+        clientRepository.save(client);
+
+        //Act
+
+        Client returnClient = clientRepository.findById(client.getId()).get();
+
+        //Asserts
+        Assertions.assertThat(returnClient).isNotNull();
+
+
+    }
+
 
 }
